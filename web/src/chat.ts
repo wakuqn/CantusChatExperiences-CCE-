a51e8c5db676a7b90ae14781d94f35acf8f58027
mirun <mirunbusiness@gmail.com>
2026-07-09 17:47:16 +0900

    add peer connection

--- web/src/chat.ts
+++ web/src/chat.ts
@@ -40,7 +40,7 @@ export function createOrJoinRoom(name: string) {
         socket.once("joined",async(members:string[]) =>{
             if (timer)clearTimeout(timer);
             for(const member of members){
-                await sendOffer(newPeerConnection, member, sendSdp);
+                await sendOffer(newPeerConnection, member ,sendSdp);
             }
 
             resolve("joined");
@@ -58,12 +58,54 @@ export function sendMessage(message: ChatMessage) {
 }
 
 function getPeer(to: string): RTCPeerConnection {
-    throw new Error("Not implemented");
+    return connects.get(to)!.chatConnection!;
 }
 
 function newPeerConnection(to: string): RTCPeerConnection {
-    throw new Error("Not implemented");
+    const peer= new RTCPeerConnection();
+    peer.onconnectionstatechange = (_) => {
+        console.log(`connection state change: ${peer.connectionState}`);
+        if (peer.connectionState === "disconnected"){
+            chatMembers.update((members) => [...members, to]);
+        }else if(
+            peer.connectionState === "disconnected" ||
+            peer.connectionState === "failed" ||
+            peer.connectionState === "closed"
+        ){
+            chatMembers.update((members) =>
+                members.filter((member) => member !== to)
+        );
+
+    };
+    peer.ondatachannel = (ev) => {
+        const{ chatConnection } = connects.get(to)!;
+        connects.set(to,{
+            chatConnection,
+            chatChannel: ev.channel,
+        });
+    };
+    const chatChannel = peer.createDataChannel("chat");
+    chatChannel.onmessage = (ev) => {
+        const message: ChatMessage = JSON.parse(ev.data);
+        console.log(message);
+        chatMessages.update((messages) => {
+            return [...messages, message];
+        });
+    };
+    peer.onicecandidate = (ev) => {
+        if(ev.candidate){
+            console.log(ev.candidate);
+            sendCandidate(ev.candidate,to,socket.id)
+
+        };
+    };
+    connects.set(to, {
+        chatConnection: peer,
+        chatChannel,
+    });
+    return peer;
 }
+
 
 function sendSdp(
     type: "offer" | "answer",
@@ -71,7 +113,7 @@ function sendSdp(
     to: string,
     from: string
 ) {
-    throw new Error("Not implemented");
+    socket.emit(type, get(roomName), to, from, JSON.stringify(sdp));
 }
 
 function sendCandidate(candidate: RTCIceCandidate, to: string, from: string) {
