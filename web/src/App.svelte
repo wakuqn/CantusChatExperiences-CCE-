9a60a86efe1dc96a0071af54650012ce11ba925c
mirun <mirunbusiness@gmail.com>
2026-07-09 18:34:51 +0900

    making ui

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -4,7 +4,6 @@
     import Snackbar from "./lib/Snackbar.svelte";
     import LeaveRoom from "./lib/LeaveRoom.svelte";
     import NewRoom from "./lib/NewRoom.svelte";
-
     let leaveRoom: LeaveRoom;
     let newRoom: NewRoom;
 </script>
